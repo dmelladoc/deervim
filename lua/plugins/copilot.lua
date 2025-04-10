beed4bd4979abeffebe47b41dfeafbd19c0c3553
Diego Mellado <dmelladoc@proton.me>
2025-04-09 22:55:36 -0400

    por algo pago copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,6 +5,9 @@ return {
             markdown = false,
             python = true,
             tex = true,
+            lua = true,
+            ["*"] = false,
         },
+        copitot_model = "gpt-4o-copilot",
     },
 }
